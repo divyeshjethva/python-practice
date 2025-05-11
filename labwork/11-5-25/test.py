885f0809dad7618e338d6fe220e19db21717de08
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-11 21:22:16 +0530

    add new file

---
+++ labwork/11-5-25/test.py
@@ -0,0 +1,9 @@
+user = input("Enter any string :")
+a = input("convert to lower/upper :")
+
+if a=="lower":
+    print(user.lower())
+elif a=="upper":
+    print(user.upper())
+else:
+    print("Enter valid opartion upper/lower")
