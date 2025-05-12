a6e971824e1bd10c6be94f4e24246751dc438ab2
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-12 15:49:11 +0530

    while loop

---
+++ labwork/12-5-25/whileLoop.py
@@ -0,0 +1,45 @@
+# user = int(input("Enter to number of line"))
+# i = 1
+# n=1
+# evenCount = 0
+# oddCount = 0
+# even = 0
+# odd = 0
+# while i <= user:
+#     n = int(input("Enter Number :"))
+#     print(n)
+#     if n%2==0:
+#         print("even")
+#         evenCount += 1
+#         even += n
+#     else:
+#         print("odd")
+#         oddCount += 1
+#         odd += n
+#     i+=1  # i = i + 1
+# print("even number total :",evenCount)
+# print("odd number total :",oddCount)
+# print("odd number sum :",even)
+# print("odd number sum :",odd)
+# print("Total sum of even or odd:", even + odd)
+
+#=======================================================
+
+# i = 1
+# fact = 1
+# while i <= 5:
+#     print(i)
+#     fact = fact * i
+#     i+=1
+# print("factorial number :", fact)
+
+#=========================================================
+
+
+i = 1
+sum = 0
+while i <= 5:
+    print(i)
+    sum = sum + i
+    i+=1
+print("Total sum is :", sum)
