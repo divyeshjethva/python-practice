830086085cd95824ec76b96add376d60f47aa34b
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-16 15:56:59 +0530

    add file

---
+++ labwork/16-5-25/task1.py
@@ -0,0 +1,35 @@
+
+#star pattern 
+
+# for i in range(1,6):
+#     print("*"*i)
+
+# for i in range(1,6):
+#     print(" "*(6-i) + "*"*i)
+
+# for i in range(1,6):
+#     print(" "*(6-i) + " *"*i)
+
+# for i in range(1,6):
+#     print("*" * (6-i))
+
+# for i in range(1,6):
+#     print(" " * i + "*" * (6-i))
+
+# for i in range(1,6):
+#     print(" " * i + " *" * (6-i))
+
+#======================================================
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*", end="")
+#     print()
+    
+    
+# for i in range(1,6):
+#     for n in range(1,i-6):
+#         print(" ", end="")
+#     for j in range(1,i+1):
+#         print("*", end="")
+#     print()
