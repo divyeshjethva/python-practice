d3b36a60c8d8041db03ebc0a105567155c487557
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-17 22:59:32 +0530

    star pattern

--- labwork/16-5-25/task1.py
+++ labwork/16-5-25/task1.py
@@ -3,21 +3,37 @@
 
 # for i in range(1,6):
 #     print("*"*i)
+#-----------------------------------
 
 # for i in range(1,6):
 #     print(" "*(6-i) + "*"*i)
+#-----------------------------------
+
 
 # for i in range(1,6):
 #     print(" "*(6-i) + " *"*i)
+#-----------------------------------
+
 
 # for i in range(1,6):
 #     print("*" * (6-i))
+#-----------------------------------
+
 
 # for i in range(1,6):
 #     print(" " * i + "*" * (6-i))
+#-----------------------------------
+
 
 # for i in range(1,6):
 #     print(" " * i + " *" * (6-i))
+#-----------------------------------
+
+
+# for i in range(5):
+#     print(" " * (5-i) + " *" * i)
+# for i in range(5):
+#     print(" " * i + " *" * (5-i))
 
 #======================================================
 
@@ -25,11 +41,65 @@
 #     for j in range(1,i+1):
 #         print("*", end="")
 #     print()
-    
-    
+
+#-----------------------------------
+
+
 # for i in range(1,6):
-#     for n in range(1,i-6):
+#     for n in range(1,6-i):
+#         print(" ", end="")
+#     for j in range(1,i):
+#         print("*", end="")
+#     print()
+
+#-----------------------------------
+
+
+# for i in range(1,6):
+#     for n in range(1,6-i):
+#         print(" ", end="")
+#     for j in range(1,i):
+#         print(" *", end="")
+#     print()
+
+#-----------------------------------
+
+
+# for i in range(1, 6):
+#     for n in range(1,6-i):
+#         print("*", end="")
+#     print()
+    
+#-----------------------------------
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for n in range(5-i):
+#         print("*", end="")
+#     print()
+
+
+#-----------------------------------
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for n in range(5-i):
+#         print(" *", end="")
+#     print()
+
+#-----------------------------------
+
+# for i in range(1,6):
+#     for n in range(1,6-i):
 #         print(" ", end="")
 #     for j in range(1,i+1):
-#         print("*", end="")
+#         print(" *", end="")
+#     print()
+# for i in range(5):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for n in range(5-i):
+#         print(" *", end="")
 #     print()
