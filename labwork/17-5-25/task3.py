8866be0cdea85ac991aacfa7f697087b543693de
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-17 23:26:18 +0530

    today task added

---
+++ labwork/17-5-25/task3.py
@@ -0,0 +1,12 @@
+
+# control statement
+for i in range(10):
+    
+    #break statement
+    # if i==5:
+    #     break
+    
+    #continue statement
+    if i == 5:
+        continue
+    print(i)
