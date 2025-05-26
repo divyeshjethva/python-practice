bca4acfb18a003cb225222f90185da19f82c5fac
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-26 11:29:05 +0530

    mid string

---
+++ classwork/26-5-25/task1.py
@@ -0,0 +1,13 @@
+# string
+
+def centerName(name):
+    s1 = len(name)//2
+    
+    if len(name)%s1==0:
+        print("Enter a even number")
+    else:
+        print(name[s1-1]+name[2]+name[s1+1])
+    
+
+name = input("Enter name :")
+centerName(name)
