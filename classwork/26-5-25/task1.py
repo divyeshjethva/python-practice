697e8296a79fb7a437b6ef5478ccbb0509084b9a
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-27 21:38:31 +0530

    find mid of string

--- classwork/26-5-25/task1.py
+++ classwork/26-5-25/task1.py
@@ -1,13 +1,12 @@
 # string
 
 def centerName(name):
-    s1 = len(name)//2
+    s1 = int(len(name)/2)
     
     if len(name)%s1==0:
-        print("Enter a even number")
+        print(name,"is a even string and enter a odd string ")
     else:
-        print(name[s1-1]+name[s1]+name[s1+1])
+        print(name[s1-1],name[s1],name[s1+1])
     
-
 name = input("Enter name :")
 centerName(name)
