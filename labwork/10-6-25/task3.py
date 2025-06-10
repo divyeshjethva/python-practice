4c400f10f4c7153b95e1056cf507614c089a4732
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-10 23:30:13 +0530

    class and filr handle

---
+++ labwork/10-6-25/task3.py
@@ -0,0 +1,12 @@
+# f = open("hhh.txt","w")
+# f.write("heloo")
+# f.write("world")
+# f.close()
+
+# f = open("hhh.txt","r")
+# print(f.read())
+# f.close()
+
+# f = open("hhh.txt","a")
+# f.write("/ngood")
+# f.close()
---
+++ labwork/10-6-25/task4.py
@@ -0,0 +1,16 @@
+class myClass:
+    def fun1(self,a,b):
+        c = a + b
+        print(c)
+        
+    def fun2(self):
+        print("morning")
+        
+    def fun3(self):
+        print("students")
+        
+obj = myClass()
+
+obj.fun1(2,3)
+obj.fun2()
+obj.fun3()
