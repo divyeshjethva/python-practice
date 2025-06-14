f5e933766e425a58b19f312cfa173995fb1fb315
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-14 10:50:30 +0530

    single inheritance

--- labwork/14-6-25/task1.py
+++ labwork/14-6-25/task1.py
@@ -4,7 +4,13 @@ class first:
     
     def fun2(self):
         print("fun 2 !!")
+
+class second:
+    def fun1(self):
+        print("second class fun 1")
     
 obj = first()
+obj1 = second()
+obj1.fun1()
 obj.fun1()
 obj.fun2()
