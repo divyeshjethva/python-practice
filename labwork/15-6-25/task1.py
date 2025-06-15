b885d5d73912fc7202e6499c17354e6afc844916
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-15 12:47:35 +0530

    oops

---
+++ labwork/15-6-25/task1.py
@@ -0,0 +1,9 @@
+class student:
+    def set_name(self,name):
+        self.name = name
+    def show_name(self):
+        print("NAME :", self.name)
+
+obj = student()
+obj.set_name("divyesh")
+obj.show_name()
