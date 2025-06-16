55fad95455942e5c23281921d462f6140ae18414
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-16 11:32:03 +0530

    multipel inheritance

--- classwork/16-6-25/task1.py
+++ classwork/16-6-25/task1.py
@@ -1,2 +1,20 @@
-print("work")
-print("hello world")
+# multipel
+
+class A:
+    def fun1(self):
+        print("method 1")
+
+class B:
+    def fun2(self):
+        print("method 2")
+
+class C(A,B):
+    def fun3(self):
+        print("method 3")
+
+obj = C()
+obj.fun3()
+obj.fun1()
+obj.fun2()
+
+
