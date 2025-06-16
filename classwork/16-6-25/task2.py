a3fd5ad53f1547763640a0e26816361ff522d6ae
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-16 11:37:42 +0530

    hirachi inheritance

---
+++ classwork/16-6-25/task2.py
@@ -0,0 +1,22 @@
+# hirachi
+
+class a:
+    def fun1(self):
+        print("method 1")
+        
+class b(a):
+    def fun2(self):
+        print("method 2")
+
+class c(a):
+    def fun3(self):
+        print("method 3")
+
+obj1 = b()
+obj2 = c()
+
+obj1.fun1()
+obj1.fun2()
+
+obj2.fun1()
+obj2.fun3()
