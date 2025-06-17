6ccea845f4813309edd25c0d21071e57d60df7f3
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-17 22:01:31 +0530

    pyramid star pattern

--- labwork/17-6-25/task1.py
+++ labwork/17-6-25/task1.py
@@ -24,7 +24,14 @@ while True:
     
     elif choice == 4:
         print("pyramid")
+        n =5
+        for i in range(1,n+1):
+            print(" " * (n-i) + " *"*i)
+
+        for j in range(1,n+1):
+            print(" " * j+" *" * (n-j))
     
     else:
         print("Thank you")
         break
+
