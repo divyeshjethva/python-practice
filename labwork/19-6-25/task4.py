90d7289eab80bae16161916f197888a2ef10966b
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-19 15:20:55 +0530

    polimorphism with hybrid

--- labwork/19-6-25/task4.py
+++ labwork/19-6-25/task4.py
@@ -1,13 +1,38 @@
 # polimorphism
 
-class a:
+# class a:
+#     def fun1(self):
+#         print("method 1")
+
+# class b(a):
+#     def fun1(self):
+#         super().fun1()
+#         print("method 2")
+    
+# obj = b()
+# obj.fun1()
+
+# ------------------------------------------------
+
+
+class dada:
     def fun1(self):
+        super().fun1()
         print("method 1")
 
-class b(a):
+class papa(dada):
     def fun1(self):
         super().fun1()
         print("method 2")
-    
-obj = b()
+
+class mom:
+    def fun1(self):
+        print("methon 3")
+
+class child(papa,mom):
+    def fun1(self):
+        super().fun1()
+        print("method 4")
+        
+obj = child()
 obj.fun1()
