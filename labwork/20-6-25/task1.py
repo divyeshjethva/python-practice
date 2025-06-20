4f36cb7ee0d8031091a3eed67ce7f18bcd5182eb
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-20 09:50:04 +0530

    find mid of String

---
+++ labwork/20-6-25/task1.py
@@ -0,0 +1,15 @@
+# find mid of String
+
+# a = "ajay"
+
+# odd = len(a) // 2 
+
+# if odd:
+#     print(a[odd-1] + a[odd] + a[odd+1])
+# else:
+#     print("Enter odd string")
+
+# print(odd)
+
+# ======================================================
+
