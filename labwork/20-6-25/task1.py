4aaf5e5a933b13ebf6bf2dbcd5b91cab969b4896
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-20 09:59:55 +0530

    count vowels

--- labwork/20-6-25/task1.py
+++ labwork/20-6-25/task1.py
@@ -13,3 +13,17 @@
 
 # ======================================================
 
+
+# Count vowels and consonants in a string - a,i,o,u,e
+
+vowels = 'aeiou'
+vowel_count = 0
+s = "divyeshee"
+
+for i in s:
+    if i in vowels:
+        vowel_count += 1
+print(vowel_count)
+
+
+
