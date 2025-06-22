9374b7569d6ce0e067e8ce675efb99dd879d458c
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-22 23:38:06 +0530

    e-commerce store start

--- labwork/22-6-25/task1.py
+++ labwork/22-6-25/task1.py
@@ -1,9 +1,25 @@
+d = {}
 class admin:
-    pass
-class user:
-    pass
+    def add(self):
+        name = input("Enter product name :")
+        price = int(input("Enter product price :"))
+        qty = int(input("Enter product qty"))
+        d[name] = {"name":name, "price":price,"qty":qty}
+        
+    def view(self):
+        for i,j in d.items():
+            print(i,":")
+            for k,v in j.items():
+                print(k,":",v)
+        
+        
+# class user:
+#     pass
 
-class join(admin,user):
-    pass
+# class join(admin,user):
+#     pass
 
-obj = join()
+obj = admin()
+
+obj.add(1)
+# for i,j in enumerate(d,1):
