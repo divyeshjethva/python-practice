31bcf876b982f4b7a6e789e82f087b5731281fad
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-23 22:36:37 +0530

    e-commerce

--- labwork/23-6-25/task3.py
+++ labwork/23-6-25/task3.py
@@ -1,2 +1,15 @@
 class admin:
-    pass
+    def add(self):
+        pname = input("Enter product name :")
+        price = input("Enter product price :")
+        qty = input("Enter product qty :")
+
+obj = admin()
+
+while True:
+    menu = """
+        press 1 for admin
+        press 2 for user
+        press 3 for exit
+    """
+    print(menu)
