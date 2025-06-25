3e218dfa9174e8c347d6b79790b1c1d1281ce8d6
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-26 01:18:43 +0530

    connection sql

---
+++ labwork/26-6-25/task3.py
@@ -0,0 +1,17 @@
+
+# import pymysql
+
+# connection = pymysql.connect (
+#     host="localhost",
+#     user="root",
+#     password="",
+#     database="my"
+# )
+# mycursor = connection.cursor()
+
+# insert_query = "INSERT INTO students (name, age, course) VALUES (%s, %s, %s)"
+# values = ("ajay", 21, 20000)
+
+# mycursor.execute(insert_query,values)
+
+# connection.commit()
