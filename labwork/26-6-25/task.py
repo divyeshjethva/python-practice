3dba8d4c4868407bd1be14c5e0c7ed7cdd433524
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-26 00:33:44 +0530

    connection sql

---
+++ labwork/26-6-25/task.py
@@ -0,0 +1,16 @@
+import pymysql
+
+db = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="my"
+)
+
+mycursor = db.cursor()
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),age INT, course VARCHAR(255))")
+mycursor.execute("INSERT INTO students (name,age,course) VALUES ('ajay',20,'BCA')")
+
+
+db.commit()
