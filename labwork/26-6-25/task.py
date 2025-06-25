7ad6c1b9a2e871b19ab2d1c150b4c15a943fbc91
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-26 00:37:48 +0530

    connection sql

--- labwork/26-6-25/task.py
+++ labwork/26-6-25/task.py
@@ -1,20 +1,20 @@
-import pymysql
+# import pymysql
 
-db = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="my"
-)
+# db = pymysql.connect(
+#     host="localhost",
+#     user="root",
+#     password="",
+#     database="my"
+# )
 
-mycursor = db.cursor()
+# mycursor = db.cursor()
 
-# mycursor.execute("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),age INT, course VARCHAR(255))")
-# mycursor.execute("INSERT INTO students (name,age,course) VALUES ('ajay',20,'BCA')")
-mycursor.execute("SELECT * FROM students")
+# # mycursor.execute("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),age INT, course VARCHAR(255))")
+# # mycursor.execute("INSERT INTO students (name,age,course) VALUES ('ajay',20,'BCA')")
+# mycursor.execute("SELECT * FROM students")
 
-rows = mycursor.fetchall()
+# rows = mycursor.fetchall()
 
-print(rows)
+# print(rows)
 
-db.commit()
+# db.commit()
