c855718c5a3523b8dcc5d730f7ee04fc409264ba
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-26 15:12:00 +0530

    bank management

--- labwork/26-6-25/task4.py
+++ labwork/26-6-25/task4.py
@@ -1,1 +1,39 @@
 # BANK MANAGEMENT SYSTEM
+
+class user:
+    def create(self):
+        pass
+    def deposit(self):
+        pass
+    def withdraw(self):
+        pass
+    def view(self):
+        pass
+
+obj = user()
+
+menu = """
+    press 1 for create account
+    press 2 for deposite account
+    press 3 for withdraw account
+    press 4 for view account
+    press 5 for exit
+"""
+
+while True:
+    print(menu)
+    choice = int(input("Enter your choice :"))
+    
+    if choice == 1:
+        obj.create()
+    elif choice == 2:
+        obj.deposit()
+    elif choice == 3:
+        obj.withdraw()
+    elif choice == 4:
+        obj.view()
+    elif choice == 5:
+        print("Thank you !!")
+        break
+    else:
+        print("invalid choice")
