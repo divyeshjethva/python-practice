5f64dd823cf897f8abec1835fd0a824c9f9cfd96
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-28 14:30:34 +0530

    sql query

---
+++ labwork/28-6-25/task2.py
@@ -0,0 +1,17 @@
+from connection import *
+
+
+
+
+
+
+# cursor.execute("SELECT * FROM bank")
+# data = cursor.fetchall()
+# # print(data)
+
+# acc = int(input("Enter account number :"))
+# for i in data:
+#     # print(i)
+#     for j in i:
+#         cursor.execute("")
+#     print("========================")
