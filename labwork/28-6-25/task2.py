ed8195194f592b02894cf5bbb6bf361a4dca660e
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-28 15:01:50 +0530

    sql query

--- labwork/28-6-25/task2.py
+++ labwork/28-6-25/task2.py
@@ -2,16 +2,35 @@ from connection import *
 
 
 
-
-
-
-# cursor.execute("SELECT * FROM bank")
-# data = cursor.fetchall()
-# # print(data)
-
+while True:
+    print("1 for update")
+    print("2 for view")
+    
+    choice = int(input("Enter choice :"))
+    if choice == 1:
+        id = int(input("Enter id:"))
+        name = input("Enter name:")
+        balance = int(input("Enter balnce :"))
+        query="UPDATE bank SET name = '%s', balance= %s WHERE id = %s"
+        value = (name,balance,id)
+        cursor.execute(query % value)
+        connection.commit()
+        
+    elif choice == 2:
+        cursor.execute("SELECT * FROM bank")
+        data = cursor.fetchall()
+        print(data)
+        connection.commit()
+        
+    else:
+        print("thank you")
+        break 
+    
 # acc = int(input("Enter account number :"))
 # for i in data:
 #     # print(i)
 #     for j in i:
 #         cursor.execute("")
 #     print("========================")
+
+connection.commit()
