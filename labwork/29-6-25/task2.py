80ea24a8adfd935ba076a4863978b8637a05e3b3
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-29 16:46:09 +0530

    update data

--- labwork/29-6-25/task2.py
+++ labwork/29-6-25/task2.py
@@ -5,3 +5,14 @@ from connection import *
 
 # cursor.execute(f"INSERT INTO test (name,age) VALUES ('{name}',{age})")
 # connection.commit()
+
+
+
+
+
+# id = int(input("Enter id"))
+# name = input("Enter your name: ")
+# age = input("Enter your age ")
+
+# cursor.execute(f"UPDATE test SET name='{name}',age='{age}' WHERE id={id}")
+# connection.commit()
