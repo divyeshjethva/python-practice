a5be359f16da04b867124f4638fd8c5c832e737e
divyeshjethva <divyeshjethva2004@gmail.com>
2025-06-29 16:40:08 +0530

    insert data

--- labwork/29-6-25/task2.py
+++ labwork/29-6-25/task2.py
@@ -1,1 +1,7 @@
 from connection import *
+
+# name = input("Enter your name: ")
+# age = input("Enter your age ")
+
+# cursor.execute(f"INSERT INTO test (name,age) VALUES ('{name}',{age})")
+# connection.commit()
