9ef2b935bc2e2688416c52b266a81aee4ad85f09
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-01 00:58:50 +0530

    todo list success ui design

---
+++ labwork/1-7-25/todo list/success.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("200x50")
+root.title("TODO LIST view all")
+
+view = Label(root,text='Successfully added',font=('Arial',15,'bold'))
+view.pack()
+
+root.mainloop()
