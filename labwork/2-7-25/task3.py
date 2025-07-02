3a2443464d0169de3940387385eaf2268855260e
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-02 15:12:56 +0530

    dropdown

--- labwork/2-7-25/task3.py
+++ labwork/2-7-25/task3.py
@@ -1,20 +1,13 @@
 from tkinter import *
 
 root = Tk()
-root.title("Simple Dropdown")
-root.geometry("250x150")
+root.geometry("200x150")
 
-choice = StringVar()
-choice.set("Select Option")
+var = StringVar()
+var.set("Choose")
 
-options = ["Option 1", "Option 2", "Option 3"]
-
-dropdown = OptionMenu(root, choice, *options)
-dropdown.pack(pady=20)
-
-def submit():
-    Label(root, text="You selected: " + choice.get()).pack()
-
-Button(root, text="Submit", command=submit).pack()
+options = ["One", "Two", "Three"]
+menu = OptionMenu(root, var, *options)
+menu.pack(pady=20)
 
 root.mainloop()
