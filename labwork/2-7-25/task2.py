eae5efb9bd5ddf04d41950b30d91b3f9d2b5a8f3
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-02 14:06:50 +0530

    frame in tkinter

---
+++ labwork/2-7-25/task2.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x500")
+root.title("Home")
+
+frame = Frame(root, bg="white", width=300, height=200, bd=5)
+frame.pack(pady=50)
+
+root.mainloop()
