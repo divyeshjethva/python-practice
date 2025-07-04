af9715ae8668b77e82e193d3b8365c83b64fa02f
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-04 12:32:19 +0530

    photo as tkinter

--- labwork/3-7-25/task1.py
+++ labwork/3-7-25/task1.py
@@ -1,35 +1,35 @@
-# from tkinter import *
-
-# x = 50
-# y = 40
-# root = Tk()
-
-# root.geometry("1200x600")
-# root.title("All products ")
-# root.configure(bg="grey")
-
-# photo = PhotoImage(file='photo.png')
-# Frame(root,height=210,width=160,borderwidth=2,relief='solid').place(x=x,y=y)
-
-# label = Label(root, image=photo)
-# label.pack()
-
-# root.mainloop()
-
-
-
 from tkinter import *
 from PIL import Image, ImageTk
+l = [599,565,233,54,123,455]
 
 root = Tk()
-root.title("Show Image")
-root.geometry("400x400")
 
-image = Image.open(rf"C:\Users\ADMIN\Desktop\backend developer\python-practice\labwork\3-7-25\gg.jpg")
-image = image.resize((300, 300)) 
-photo = ImageTk.PhotoImage(image)
+root.geometry("1200x600")
+root.title("All products ")
+root.configure(bg="grey")
 
-label = Label(root, image=photo)
-label.pack()
+def product(path,i,x,y):
+    Frame(root,height=220,width=160,borderwidth=2,relief='solid').place(x=x,y=y)
 
+    image = Image.open(path)
+    image = image.resize((152,145)) 
+    photo = ImageTk.PhotoImage(image)
+
+    label = Label(root, image=photo)
+    label.image = photo
+    label.place(x=x+2,y=y+2)
+    
+    Label(root,text=f"$ {i}",font=('Arial',12)).place(x=x+5,y=y+150)
+    
+    Button(root,text="Add to card",bg='green',fg='white',font=('Arial',10,'bold'),width=16).place(x=x+10,y=y+187)
+
+path = rf"C:\Users\ADMIN\Desktop\backend developer\python-practice\labwork\3-7-25\gg.jpg"
+
+x = 50 
+y = 40
+for i in l:
+    product(path,i,x,y)
+    x = x + 180
+    
 root.mainloop()
+
